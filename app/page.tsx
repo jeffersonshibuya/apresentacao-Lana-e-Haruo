c27856573b92b51fd55379fab810cb556fee2965
Jefferson R. Shibuya <jeffersonshibuya@yahoo.com.br>
2024-12-11 10:21:18 -0300

    back audio

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen w-screen relative overflow-hidden">
-      {/* <AudioPlayer /> */}
+      <AudioPlayer />
       <Header />
       <div className="pb-8">
         <FloatingDockMenu />
